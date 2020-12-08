49b62e3791cec5eb1293cbb38bc1425a57d201bb
kondrashev-y <70172501+kondrashev-y@users.noreply.github.com>
2020-12-08 23:09:32 +0300

    add breadcrumbs

--- dj_project/movies/models.py
+++ dj_project/movies/models.py
@@ -17,6 +17,9 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+    def get_absolute_url(self):
+        return reverse("category_list", kwargs={"slug": self.url})
+
 
 class Actor(models.Model):
     """Актеры и режессеры"""
--- dj_project/movies/templatetags/breadcrumps_tag.py
+++ dj_project/movies/templatetags/breadcrumps_tag.py
@@ -1,38 +1,54 @@
 from django import template
+from django.urls import reverse
 
-from ..models import Movie, Category
-from ..views import MoviesView
+from ..models import Category
 
 register = template.Library()
 
 
+class ActorList:
+    """Класс для yrl Актеров и Режиссеров"""
+    get_absolute_url = reverse('actor_list')
+
+    def __str__(self):
+        return 'Актеры и Режиссеры'
+
+
+class RatingList:
+    """Класс для yrl Актеров и Режиссеров"""
+    get_absolute_url = '#'   # заглушка вместо  reverse('rating_list') пока нет шаблона для рейтинга
+
+    def __str__(self):
+        return 'Рэйтинг'
+
+
 @register.inclusion_tag('breadcrumbs_template.html', takes_context=True)
-def breadcrumbs(context, obj=None, status=None):
+def breadcrumbs(context):
 
     crumbs = []
-    if status:
-        crumbs.append(status)
+    path = context.get('request').path
     if context.get('movie'):
-        crumbs.append(context['movie'].category)
-        crumbs.append(context['movie'])
-    if '/category/' in context.get('request').path:
+        model = context['movie']
+        crumbs.append(model.category)
+        crumbs.append(model)
+    elif '/category/' in path:
         crumbs.append(get_category(context['view'].kwargs.get('slug')))
-    if '/filter/' in context.get('request').path:
+    elif '/filter/' in path:
         crumbs.append('Фильтр')
-    elif 'search/' in context.get('request').path:
+    elif 'search/' in path:
         crumbs.append('Поиск')
-    if 'actor' in context.get('request').path:
-        crumbs.append('Актеры и Режиссеры')
-    if context.get('actor'):
-        crumbs.append(context['actor'])
-
-    # print(context)
-    # print(context['view'].kwargs.get('slug'))
-    # print(context['request'].path)
-    # print(context['actor'])
+    elif 'actor' in path:
+        crumbs.append(ActorList)
+        if context.get('actor'):
+            crumbs.append(context['actor'])
+    elif '/rating/' in path:
+        crumbs.append(RatingList)
+        crumbs.append(context['view'].kwargs.get('rt'))
     return {'crumbs': crumbs}
 
 
 def get_category(slug):
     category_name = Category.objects.get(url=slug)
     return category_name
+
+
